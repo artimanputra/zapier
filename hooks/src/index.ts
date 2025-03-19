aab27103ba69972cabd218e2840559cb8699aea5
Arti Manputra <aartimanputra20@gmail.com>
2025-03-19 19:48:12 +0530

    added new files

---
+++ hooks/src/index.ts
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app= express();
+
+//https://hooks.zapier.com/hooks/catch/22127173/2ljegog/
+
+app.post("/hooks/catch/:userId/:zapId", (req, res) => {
+    const userId = req.params.userId;
+    const zapId = req.params.zapId;
+  console.log(req.body);    
+});
