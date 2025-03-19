9df90017058447ed13fa3a5e9c8e70ae4dc6f269
Arti Manputra <aartimanputra20@gmail.com>
2025-03-19 20:00:17 +0530

    initial commit

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 
-const app= express();
+const app = express();
 
 //https://hooks.zapier.com/hooks/catch/22127173/2ljegog/
 
-app.post("/hooks/catch/:userId/:zapId", (req, res) => {
-    const userId = req.params.userId;
-    const zapId = req.params.zapId;
-  console.log(req.body);    
+app.post ('/hooks/catch/:userId/:zapId', (req, res) => {
+    const userId= req.params.userId;
+    const zapId= req.params.zapId; 
+    console.log(req.body);
+    res.send('Received');
 });
